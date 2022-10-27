63f45cc4de040ca86f9f10411d46283552aa5f3c
xiaoxue <>
2022-10-27 15:13:44 +0800

    emit不只传一个参数

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,16 @@
 class EventHub {
-  cache = {};
+  private cache = {};
   // {
   //   '楚天都市报':[fn1,fn2,fn3],
   //   '羊城晚报':[fn1,fn2,fn3],
   // }
   on(eventName: string, fn: any) {
-    //把fn推进this.cache[eventName]数组
-    //eventName = 楚天,fn
+    //把fn推进this.cache[eventName]数组  eventName = 楚天,fn
     this.cache[eventName] = this.cache[eventName] || [];
     this.cache[eventName].push(fn);
   }
   emit(eventName: string, data?) {
     //把this.cache[eventName]里面的fn全部依次调用
-    if (this.cache[eventName] === undefined) return;
     (this.cache[eventName] || []).forEach((fn) => fn(data));
   }
   off(eventName: string, fn) {
@@ -25,6 +23,12 @@ class EventHub {
 
 export default EventHub;
 
+/**
+ * 帮助函数 indexOf
+ * @param array 
+ * @param item 
+ * @returns 
+ */
 function indexOf(array, item) {
   if (array === undefined) return -1;
   let index = -1;
--- test/index.ts
+++ test/index.ts
@@ -1,26 +1,46 @@
 import EventHub from "../src/index";
 
-const eventHub1 = new EventHub()
-console.assert(eventHub1 instanceof Object === true,'eventHub是个对象')
+const test1 = (message) => {
+  const eventHub = new EventHub();
+  console.assert(eventHub instanceof Object === true, "eventHub是个对象");
+  console.log(message);
+};
 //on emit
-let called = false
-eventHub1.on('xxx',(y)=>{
-  called = true
-  console.log('called',called)
-  console.log(y)
-  console.assert(y === '今天降温了')
-})
-eventHub1.emit('xxx','今天降温了')
-
-const eventHub2 = new EventHub()
-//on emit
-let called2 = false
-const fn = ()=>{
-  called2 = true
+const test2 = (message) => {
+  const eventHub = new EventHub();
+  let called = false;
+  eventHub.on("xxx", (y) => {
+    called = true;
+    console.log('y',y);
+    console.assert(y[0] === "今天降温了");
+    console.assert(y[1] === "我今天穿了毛衣");
+  });
+  eventHub.emit("xxx", ["今天降温了","我今天穿了毛衣"]);
+  setTimeout(() => {
+    console.log("called", called);
+    console.assert(called === true);
+    console.log(message);
+  }, 1000);
 }
-eventHub2.on('yyy',fn)
-eventHub2.off('yyy',fn)
-eventHub2.emit('yyy')
-setTimeout(()=>{
-  console.log('called2',called2)
-},1000)
+
+const test3 = (message) => {
+  const eventHub = new EventHub();
+  //on emit
+  let called2 = false;
+  const fn = () => {
+    called2 = true;
+  };
+  eventHub.on("yyy", fn);
+  eventHub.off("yyy", fn);
+  eventHub.emit("yyy");
+  setTimeout(() => {
+    console.log("called2", called2);
+    console.assert(called2 === false);
+    console.log(message);
+  }, 1000);
+}
+
+test1('EventHub可以创建对象')
+test2('.on了之后 .emit 会触发 .on的函数')
+test3('.off有用')
+
