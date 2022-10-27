b45f7c2c4953d62fa8495ba2e7b32ba1f409665d
xiaoxue <>
2022-10-27 14:33:55 +0800

    添加off函数

--- src/index.ts
+++ src/index.ts
@@ -8,12 +8,27 @@ class EventHub {
     //把fn推进this.cache[eventName]数组
     //eventName = 楚天,fn
     this.cache[eventName] = this.cache[eventName] || [];
-    const array = this.cache[eventName].push(fn);
+    this.cache[eventName].push(fn);
   }
   emit(eventName: string, data?) {
     //把this.cache[eventName]里面的fn全部依次调用
     if (this.cache[eventName] === undefined) return;
     (this.cache[eventName] || []).forEach((fn) => fn(data));
+  }
+  off(eventName: string,fn){
+    if (this.cache[eventName] === undefined) return;
+    let index
+    for(let i=0;i<this.cache[eventName].length;i++){
+      if(this.cache[eventName][i] === fn){
+        index = i;
+        break;
+      }
+    }
+    if(index === undefined){
+      return;
+    }else {
+      this.cache[eventName].splice(index, 1);
+    }
   }
 }
 
--- test/index.ts
+++ test/index.ts
@@ -1,17 +1,26 @@
 import EventHub from "../src/index";
 
-console.log(EventHub)
-
-const eventHub = new EventHub()
-
-console.assert(eventHub instanceof Object === true,'eventHub是个对象')
-
+const eventHub1 = new EventHub()
+console.assert(eventHub1 instanceof Object === true,'eventHub是个对象')
 //on emit
 let called = false
-eventHub.on('xxx',(y)=>{
+eventHub1.on('xxx',(y)=>{
   called = true
   console.log('called',called)
   console.log(y)
   console.assert(y === '今天降温了')
 })
-eventHub.emit('xxx','今天降温了')
+eventHub1.emit('xxx','今天降温了')
+
+const eventHub2 = new EventHub()
+//on emit
+let called2 = false
+const fn = ()=>{
+  called2 = true
+}
+eventHub2.on('yyy',fn)
+eventHub2.off('yyy',fn)
+eventHub2.emit('yyy')
+setTimeout(()=>{
+  console.log('called2',called2)
+},1000)
