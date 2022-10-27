3e522b05dbd7b2eddb0aedd3a9cbaf0d52e46017
xiaoxue <>
2022-10-27 14:18:33 +0800

    优化代码

--- src/index.ts
+++ src/index.ts
@@ -4,22 +4,16 @@ class EventHub {
   //   '楚天都市报':[fn1,fn2,fn3],
   //   '羊城晚报':[fn1,fn2,fn3],
   // }
-  on(eventName:string, fn:any) {
+  on(eventName: string, fn: any) {
     //把fn推进this.cache[eventName]数组
     //eventName = 楚天,fn
-    if (this.cache[eventName] === undefined) {
-      this.cache[eventName] = []; //初始化
-    }
-    const array = this.cache[eventName];
-    array.push(fn);
+    this.cache[eventName] = this.cache[eventName] || [];
+    const array = this.cache[eventName].push(fn);
   }
-  emit(eventName:string) {
+  emit(eventName: string, data?) {
     //把this.cache[eventName]里面的fn全部依次调用
-    let array = this.cache[eventName];
-    if (array === undefined) {
-      array = []; //作保底
-    }
-    array.forEach((fn) => fn());
+    if (this.cache[eventName] === undefined) return;
+    (this.cache[eventName] || []).forEach((fn) => fn(data));
   }
 }
 
--- test/index.ts
+++ test/index.ts
@@ -4,12 +4,14 @@ console.log(EventHub)
 
 const eventHub = new EventHub()
 
-console.assert(eventHub instanceof Object === false,'eventHub是个对象')
+console.assert(eventHub instanceof Object === true,'eventHub是个对象')
 
 //on emit
 let called = false
-eventHub.on('xxx',()=>{
+eventHub.on('xxx',(y)=>{
   called = true
   console.log('called',called)
+  console.log(y)
+  console.assert(y === '今天降温了')
 })
-eventHub.emit('xxx')
+eventHub.emit('xxx','今天降温了')
